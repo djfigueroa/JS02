bf9696d35452650514bbf9b089ce7b9f5d5b2d74
djfigueroa <d.figueroa10452@gmail.com>
2017-10-27 19:41:55 -0400

    Chanllenege 5
    Challenege 5, Data. html, Data.css, Data.js

---
+++ Challenge 5/Data.js
@@ -0,0 +1,98 @@
+ $('#number').append(`<tr>
+
+          <th>myInt</th>
+          <td><input type="number" id="myInt"></td>
+        </tr>
+        
+        <tr>
+        <th>myString</th>
+        <td> The number
+        <input type="number" id="myString">
+        </td>
+      </tr>
+      
+      <tr>
+          <th>myNumString</th>
+          <td><input type="number" id="myNumString"></td>
+      </tr>`);
+
+/*var myInt = $("#myInt")
+var myString = $("#myString")
+var myNumString = $("#myNumString")
+var answer1 = myInt.val() + myString.val()
+
+$("button").click( function() {
+ $("p").text(answer1);
+});
+console.log(answer1)
+*/
+$('#answer').append(`
+
+      <th id="color">Addition</th>
+
+      <tr>
+          <th>myInt + myInt?</th>
+          <td></td>
+        </tr>
+
+      <tr>
+          <th>myInt + myString?</th>
+          <td></td>
+        </tr>
+
+      <tr>
+          <th>myInt + myNumString?</th>
+          <td></td>
+        </tr>
+
+      <tr>
+          <th>myNumString + myInt?</th>
+          <td></td>
+        </tr>
+
+        <th id="color">Fancier addition</th>
+
+      <tr>
+          <th>myInt + myNumString + myString?</th>
+          <td></td>
+        </tr>
+
+      <tr>
+          <th>myInt + myInt + myString?</th>
+          <td></td>
+        </tr>
+
+        <tr>
+          <th>myString + myInt + myInt?</th>
+          <td></td>
+        </tr>
+
+        <th id="color">Subtraction</th>
+
+        <tr>
+          <th>myInt - myString?</th>
+          <td></td>
+        </tr>
+
+        <tr>
+          <th>myInt - myNumString?</th>
+          <td></td>
+        </tr>
+
+        <tr>
+          <th>myString - myNumString?</th>
+          <td></td>
+        </tr>
+
+        <th id="color">Comparisons</th>
+
+        <tr>
+          <th>myInt == myNumString?</th>
+          <td></td>
+        </tr>
+
+        <tr>
+          <th>myInt === myNumString?</th>
+          <td></td>
+        </tr>   
+  `);
