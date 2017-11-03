b19fecee6ffdb6a9f51f3fa5b01506f2a9c71e6c
djfigueroa <d.figueroa10452@gmail.com>
2017-11-03 19:07:12 -0400

    Changed one append to multiple to make the table work
    Made the one append to make the table into multiple appends for each table row, header and, message.

--- Challenge 5/Data.js
+++ Challenge 5/Data.js
@@ -1,98 +1,111 @@
- $('#number').append(`<tr>
 
+$(document).ready(function(){
+  $('#number').append(`
+        <tr>
           <th>myInt</th>
           <td><input type="number" id="myInt"></td>
         </tr>
         
-        <tr>
+      <tr>
         <th>myString</th>
         <td> The number
-        <input type="number" id="myString">
+          <input type="number" id="myString">
         </td>
       </tr>
       
       <tr>
-          <th>myNumString</th>
-          <td><input type="number" id="myNumString"></td>
-      </tr>`);
+        <th>myNumString</th>
+        <td><input type="number" id="myNumString"></td>
+      </tr> 
+  `); 
 
-/*var myInt = $("#myInt")
+var myInt = $("#myInt")
 var myString = $("#myString")
 var myNumString = $("#myNumString")
 var answer1 = myInt.val() + myString.val()
 
 $("button").click( function() {
- $("p").text(answer1);
+  $("#p1").append(answer1);
+  console.log(myInt.val())
+  console.log(myString.val())
 });
-console.log(answer1)
-*/
+
+
 $('#answer').append(`
 
-      <th id="color">Addition</th>
+    <th id="color">Addition</th>
 
-      <tr>
-          <th>myInt + myInt?</th>
-          <td></td>
-        </tr>
+    <tr>
+        <th>myInt + myString?</th>
+        <td></td>
+      </tr>
+`);
 
-      <tr>
-          <th>myInt + myString?</th>
-          <td></td>
-        </tr>
-
-      <tr>
+    $('#answer').append(`<tr>
           <th>myInt + myNumString?</th>
           <td></td>
-        </tr>
+        </tr>`);
 
-      <tr>
+    $('#answer').append(`       
+        <tr>
           <th>myNumString + myInt?</th>
           <td></td>
-        </tr>
+        </tr>`);
 
-        <th id="color">Fancier addition</th>
+    $('#answer').append(`        
+      <th id="color">Fancier addition</th>`);
 
-      <tr>
+    $('#answer').append(`        
+        <tr>
           <th>myInt + myNumString + myString?</th>
           <td></td>
-        </tr>
+        </tr>`);
 
-      <tr>
+    $('#answer').append(`
+        <tr>
           <th>myInt + myInt + myString?</th>
           <td></td>
-        </tr>
+        </tr>`);
 
+    $('#answer').append(`        
         <tr>
           <th>myString + myInt + myInt?</th>
           <td></td>
-        </tr>
+        </tr>`);
 
-        <th id="color">Subtraction</th>
+    $('#answer').append(`       
+     <th id="color">Subtraction</th>`);
 
+    $('#answer').append(`        
         <tr>
           <th>myInt - myString?</th>
           <td></td>
-        </tr>
+        </tr>`);
 
+    $('#answer').append(`        
         <tr>
           <th>myInt - myNumString?</th>
           <td></td>
-        </tr>
+        </tr>`);
 
+    $('#answer').append(`        
         <tr>
           <th>myString - myNumString?</th>
           <td></td>
-        </tr>
+        </tr>`);
 
-        <th id="color">Comparisons</th>
+    $('#answer').append(`
+      <th id="color">Comparisons</th>`);
 
+    $('#answer').append(` 
         <tr>
           <th>myInt == myNumString?</th>
           <td></td>
-        </tr>
+        </tr>`);
 
+    $('#answer').append(`        
         <tr>
           <th>myInt === myNumString?</th>
           <td></td>
-        </tr>   
-  `);
+        </tr> `);
+});
